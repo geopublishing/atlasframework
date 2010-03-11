f200d578bb6e46b24854b9ec26102299e0d5fe91
alfonx <tzeggai@wikisquare.de>
2010-03-11 08:48:34 +0000

    AS: Allow the user to select an alternative .sld prosition when the imported .shp is read-only.

--- src/skrueger/sld/AtlasStylerSaveLayerToSLDAction.java
+++ src/skrueger/sld/AtlasStylerSaveLayerToSLDAction.java
@@ -12,9 +12,13 @@ package skrueger.sld;
 
 import java.awt.Component;
 import java.awt.event.ActionEvent;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import javax.swing.AbstractAction;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
@@ -22,8 +26,10 @@ import org.apache.log4j.Logger;
 import schmitzm.geotools.styling.StylingUtil;
 import schmitzm.io.IOUtil;
 import schmitzm.swing.ExceptionDialog;
+import schmitzm.swing.SwingUtil;
 import skrueger.atlas.AVUtil;
 import skrueger.atlas.gui.plaf.BasicMapLayerLegendPaneUI;
+import skrueger.creator.AtlasCreator;
 import skrueger.geotools.StyledFS;
 
 public class AtlasStylerSaveLayerToSLDAction extends AbstractAction {
@@ -49,13 +55,47 @@ public class AtlasStylerSaveLayerToSLDAction extends AbstractAction {
 
 		boolean backup = false;
 
+		// Test whether a .sld file can be created. Ask the user to change
+		// position of the .sld
+		while (! IOUtil.canWriteOrCreate(styledShp.getSldFile()) ) {
+
+			AVUtil.showMessageDialog(owner, AtlasStyler.R(
+					"StyledLayerSLDNotWritable.Msg", styledShp.getSldFile().getAbsolutePath()));
+
+			File startWithDir = new File(System.getProperty("user.home"),
+					styledShp.getSldFile().getName());
+			JFileChooser dc = new JFileChooser(startWithDir);
+			dc.addChoosableFileFilter(new FileNameExtensionFilter("SLD",
+					new String[] { "sld", "xml" }));
+			dc.setDialogType(JFileChooser.SAVE_DIALOG);
+			dc.setDialogTitle(AtlasCreator
+					.R("StyledLayerSLDNotWritable.ChooseNewDialog.Title"));
+			dc.setSelectedFile(startWithDir);
+
+			if ((dc.showSaveDialog(owner) != JFileChooser.APPROVE_OPTION)
+					|| (dc.getSelectedFile() == null))
+				return;
+
+			File exportFile = dc.getSelectedFile();
+
+			if (!(exportFile.getName().toLowerCase().endsWith("sld") || exportFile
+					.getName().toLowerCase().endsWith("xml"))) {
+				exportFile = new File(exportFile.getParentFile(), exportFile
+						.getName()
+						+ ".sld");
+			}
+
+			styledShp.setSldFile(exportFile);
+		}
+
 		if (styledShp.getSldFile().exists()) {
-			backup = true;
 			try {
 				FileUtils.copyFile(styledShp.getSldFile(), IOUtil
 						.changeFileExt(styledShp.getSldFile(), "sld.bak"));
+				backup = true;
 			} catch (IOException e1) {
-				ExceptionDialog.show(owner, e1);
+				LOGGER.warn("could not create a backup of the existing .sld",
+						e1);
 				return;
 			}
 		}
@@ -74,7 +114,7 @@ public class AtlasStylerSaveLayerToSLDAction extends AbstractAction {
 								.getSldFile().getAbsolutePath()));
 
 		} catch (Exception e1) {
-			LOGGER.error(e1);
+			LOGGER.error("saveStyleToSLD", e1);
 			ExceptionDialog.show(owner, e1);
 			return;
 		}
