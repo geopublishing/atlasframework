638fde142d2827654b493d8017f4b4e4fff2eb93
alfonx <tzeggai@wikisquare.de>
2010-03-30 13:33:28 +0000

    erster commit der neuen kleine maven projecte

---
+++ ascore/src/main/java/org/geopublishing/atlasStyler/GraduatedColorRuleList.java
@@ -0,0 +1,356 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Stefan A. Krüger (soon changing to Stefan A. Tzeggai).
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser Public License v2.1
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * 
+ * Contributors:
+ *     Stefan A. Krüger (soon changing to Stefan A. Tzeggai) - initial API and implementation
+ ******************************************************************************/
+package org.geopublishing.atlasStyler;
+
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.geopublishing.atlasStyler.classification.QuantitiesClassification;
+import org.geopublishing.atlasStyler.classification.QuantitiesClassification.METHOD;
+import org.geotools.brewer.color.BrewerPalette;
+import org.geotools.brewer.color.PaletteType;
+import org.geotools.styling.FeatureTypeStyle;
+import org.geotools.styling.Rule;
+import org.geotools.styling.Symbolizer;
+import org.opengis.filter.Filter;
+import org.opengis.filter.expression.Expression;
+import org.opengis.filter.expression.Literal;
+
+import skrueger.AttributeMetadataImpl;
+import skrueger.geotools.StyledFeaturesInterface;
+
+/**
+ * 
+ * This abstract class represents a list of {@link Rule Rules} meant to style a
+ * quantity with graduating colors.
+ * 
+ * @author Stefan A. Krüger
+ * 
+ */
+public abstract class GraduatedColorRuleList extends QuantitiesRuleList<Double> {
+	private static final Logger LOGGER = Logger
+			.getLogger(GraduatedColorRuleList.class);
+	/** KEY-name for the KVPs in the meta information * */
+	private static final String KVP_METHOD = "METHOD";
+
+	/** KEY-name for the KVPs in the meta information * */
+	private static final String KVP_PALTETTE = "PALETTE";
+
+	/**
+	 * The {@link BrewerPalette} used in this {@link QuantitiesRuleList}
+	 */
+	private BrewerPalette brewerPalette = ASUtil.getPalettes(new PaletteType(
+			true, false), -1)[0];
+
+	protected METHOD method = QuantitiesClassification.DEFAULT_METHOD;
+
+	/**
+	 * This {@link RuleChangeListener} is added to the template in
+	 * {@link #getTemplate()} and will propagate any template changes to this
+	 * {@link GraduatedColorRuleList}
+	 */
+	private RuleChangeListener listenToTemplateRLChangesAndPropageToGraduateColorsRL = new RuleChangeListener() {
+
+		@Override
+		public void changed(RuleChangedEvent e) {
+			GraduatedColorRuleList.this.fireEvents(new RuleChangedEvent(
+					"template changed", GraduatedColorRuleList.this));
+		}
+	};
+
+	public GraduatedColorRuleList(StyledFeaturesInterface<?> styledFeatures) {
+		super(styledFeatures);
+	}
+
+	public METHOD getMethod() {
+		return method;
+	}
+
+	/**
+	 * Overriding it to add a listener that will automatically propagate the
+	 * rulelistechange to the graduate color rule list.
+	 */
+	@Override
+	public SingleRuleList<? extends Symbolizer> getTemplate() {
+		SingleRuleList<? extends Symbolizer> temp = super.getTemplate();
+
+		// We may add this listener as often as we want, because the listeners
+		// are registered in a WeakHashSet
+		temp.addListener(listenToTemplateRLChangesAndPropageToGraduateColorsRL);
+		return temp;
+	}
+
+	@Override
+	public List<Rule> getRules() {
+
+		ArrayList<Double> classLimitsAsArray = getClassLimitsAsArrayList();
+
+		if (classLimitsAsArray.size() == 0) {
+			return new ArrayList<Rule>();
+		}
+
+		ArrayList<Rule> rules = new ArrayList<Rule>();
+
+		Literal lowerRange = null;
+		Literal upperRange = null;
+		Filter filter = null;
+
+		for (int i = 0; i < getNumClasses(); i++) {
+			Rule rule;
+
+			Expression value;
+			if (getNormalizer_field_name() == null) {
+				value = ff2.property(getValue_field_name());
+				lowerRange = ff2.literal(classLimitsAsArray.get(i));
+				upperRange = ff2.literal(classLimitsAsArray.get(i + 1));
+
+			} else {
+				value = ff2.divide(ff2.property(getValue_field_name()), ff2
+						.property(getNormalizer_field_name()));
+
+				// If we have a normalizing field, we always go for double
+				lowerRange = ff2.literal(classLimitsAsArray.get(i));
+				upperRange = ff2.literal(classLimitsAsArray.get(i + 1));
+			}
+
+			filter = ff2.between(value, lowerRange, upperRange);
+
+			SingleRuleList<? extends Symbolizer> clone = getTemplate().copy();
+
+			final Color newColor = getColors()[i];
+
+			clone.setColor(newColor);
+
+			rule = clone.getRule();
+
+			// Exclude the NODATA values for the rule
+			if (getNoDataFilter() != Filter.EXCLUDE)
+				filter = ff2.and(ff2.not(getNoDataFilter()), filter);
+
+			rule.setFilter(filter);
+
+			rule.setTitle((String) getRuleTitles().get(i));
+
+			rule.setName("AS: " + (i + 1) + "/" + (getClassLimits().size() - 1)
+					+ " " + this.getClass().getSimpleName());
+
+			rules.add(rule);
+
+		}
+
+		// The last rule(s) are the NODATA rules
+
+		{
+			SingleRuleList<? extends Symbolizer> copyOfNoDataSymbol = getNoDataSymbol()
+					.copy();
+			List<Rule> rules2 = copyOfNoDataSymbol.getRules();
+			for (Rule noDataRule : rules2) {
+
+				if (copyOfNoDataSymbol.isVisibleInLegend())
+					noDataRule
+							.setName(FeatureRuleList.NODATA_RULE_NAME_SHOWINLEGEND);
+				else
+					noDataRule
+							.setName(FeatureRuleList.NODATA_RULE_NAME_HIDEINLEGEND);
+
+				noDataRule.setFilter(getNoDataFilter());
+				rules.add(noDataRule);
+			}
+		}
+
+		return rules;
+	}
+
+	public BrewerPalette getBrewerPalette() {
+
+		/**
+		 * For whatever reason, the brewerPalette.getPaletteSuitability() can be
+		 * greater than brewerPalette.getMaxColors(). So we try that first.
+		 */
+		int maxColors = brewerPalette.getPaletteSuitability() != null ? brewerPalette
+				.getPaletteSuitability().getMaxColors()
+				: brewerPalette.getMaxColors();
+
+		if (getNumClasses() > maxColors) {
+
+			LOGGER.info("Reducing the Number of classes from "
+					+ getNumClasses() + " to " + maxColors
+					+ " because we don't have a betterPalette");
+		}
+
+		return brewerPalette;
+	}
+
+	public void setBrewerPalette(BrewerPalette newPalette) {
+
+		// Only react to real changes
+		if (newPalette.getDescription() != null
+				&& newPalette.getDescription().equals(
+						brewerPalette.getDescription())) {
+			return;
+		}
+
+		this.brewerPalette = newPalette;
+		setColors(null);
+		fireEvents(new RuleChangedEvent("Set brewer palette", this));
+	}
+
+	/**
+	 * @return An Array of Colors for the classes. The length of the array
+	 *         equals the number of classes.
+	 */
+	@Override
+	public Color[] getColors() {
+
+		if (super.getColors() == null) {
+			if (getNumClasses() > getBrewerPalette().getMaxColors()) {
+				throw new RuntimeException(" numClasses (" + getNumClasses()
+						+ ") > getBrewerPalette().getMaxColors() ("
+						+ getBrewerPalette().getMaxColors() + ")");
+			}
+
+			setColors(getBrewerPalette().getColors(getNumClasses()));
+		}
+
+		final Color[] colors = super.getColors();
+		return colors;
+	}
+
+	public void setMethod(METHOD method) {
+		this.method = method;
+	}
+
+	/**
+	 * Together with {@link #parseMetaInfoString(String, FeatureTypeStyle)} this
+	 * allows loading and saving the RL
+	 */
+	@Override
+	public String getAtlasMetaInfoForFTSName() {
+		String metaInfoString = getTypeID().toString();
+
+		metaInfoString = extendMetaInfoString(metaInfoString);
+
+		metaInfoString += METAINFO_SEPERATOR_CHAR + KVP_METHOD
+				+ METAINFO_KVP_EQUALS_CHAR + getMethod();
+
+		metaInfoString += METAINFO_SEPERATOR_CHAR + KVP_PALTETTE
+				+ METAINFO_KVP_EQUALS_CHAR + getBrewerPalette().getName();
+
+		// LOGGER.debug("metainfo= " + metaInfoString);
+
+		return metaInfoString;
+	}
+
+	/**
+	 * Together with {@link #getAtlasMetaInfoForFTSName()} this allows loading
+	 * and saving the RL
+	 */
+	@Override
+	public void parseMetaInfoString(String metaInfoString,
+			FeatureTypeStyle importFTS) {
+
+		metaInfoString = metaInfoString.substring(getTypeID().toString()
+				.length());
+
+		super.parseMetaInfoString(metaInfoString);
+
+		/***********************************************************************
+		 * Parsing a list of Key-Value Pairs from the FeatureTypeStyleName
+		 */
+		String[] params = metaInfoString.split(METAINFO_SEPERATOR_CHAR);
+		for (String p : params) {
+			String[] kvp = p.split(METAINFO_KVP_EQUALS_CHAR);
+
+			if (kvp[0].equalsIgnoreCase(KVP_METHOD.toString())) {
+
+				// We had a typo error in AtlasStyler 1.1 - to correctly import
+				// old styled, we have to correct it here:
+				if (kvp[1].equals("QANTILES"))
+					kvp[1] = "QUANTILES";
+
+				setMethod(METHOD.valueOf(kvp[1]));
+
+			}
+
+			else
+
+			if (kvp[0].equalsIgnoreCase(KVP_PALTETTE)) {
+				String brewerPaletteName = kvp[1];
+
+				BrewerPalette foundIt = null;
+
+				for (BrewerPalette ppp : ASUtil.getPalettes(new PaletteType(
+						true, false), getNumClasses())) {
+					if (ppp.getName().equals(brewerPaletteName)) {
+						foundIt = ppp;
+						break;
+					}
+				}
+				if (foundIt == null) {
+					LOGGER.warn("Couldn't find the palette with the name '"
+							+ brewerPaletteName + "'.");
+				} else {
+					setBrewerPalette(foundIt);
+				}
+			}
+
+		}
+
+		/***********************************************************************
+		 * Determining the template by analyzing the first rule
+		 */
+		importTemplate(importFTS);
+
+	}
+
+	/**
+	 * Return the {@link Filter} that will catch all NODATA values.
+	 */
+	@Override
+	public Filter getNoDataFilter() {
+
+		// Checking the value attribute for NODATA values
+		String attributeLocalName = getValue_field_name();
+		AttributeMetadataImpl amd1 = getStyledFeatures().getAttributeMetaDataMap()
+				.get(attributeLocalName);
+
+		List<Filter> ors = new ArrayList<Filter>();
+		ors.add(ff2.isNull(ff2.property(attributeLocalName)));
+		if (amd1 != null && amd1.getNodataValues() != null)
+			for (Object ndValue : amd1.getNodataValues()) {
+				ors.add(ff2.equals(ff2.property(attributeLocalName), ff2
+						.literal(ndValue)));
+			}
+
+		// Checking the normalization attribute for NODATA values
+		String normalizerLocalName = getNormalizer_field_name();
+		if (normalizerLocalName != null) {
+			AttributeMetadataImpl amd2 = getStyledFeatures()
+					.getAttributeMetaDataMap().get(normalizerLocalName);
+
+			ors.add(ff2.isNull(ff2.property(normalizerLocalName)));
+			
+			// As we are dividing by this value, always add the zero also!
+			ors.add(ff2.equals(ff2.property(normalizerLocalName),ff2.literal(0)));
+			
+			if (amd2 != null && amd2.getNodataValues() != null)
+				for (Object ndValue : amd2.getNodataValues()) {
+					ors.add(ff2.equals(ff2.property(normalizerLocalName), ff2
+							.literal(ndValue)));
+				}
+		}
+
+		return ff2.or(ors);
+	}
+
+}
---
+++ ascore/src/main/java/org/geopublishing/atlasStyler/QuantitiesRuleList.java
@@ -0,0 +1,390 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Stefan A. Krüger (soon changing to Stefan A. Tzeggai).
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser Public License v2.1
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * 
+ * Contributors:
+ *     Stefan A. Krüger (soon changing to Stefan A. Tzeggai) - initial API and implementation
+ ******************************************************************************/
+package org.geopublishing.atlasStyler;
+
+import java.awt.Color;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.TreeSet;
+
+import org.apache.log4j.Logger;
+import org.geopublishing.atlasStyler.AtlasStyler.LANGUAGE_MODE;
+import org.geotools.filter.AndImpl;
+import org.geotools.styling.FeatureTypeStyle;
+import org.opengis.filter.Filter;
+import org.opengis.filter.PropertyIsBetween;
+
+import schmitzm.geotools.feature.FeatureUtil;
+import schmitzm.geotools.styling.StylingUtil;
+import schmitzm.swing.SwingUtil;
+import skrueger.geotools.StyledFeaturesInterface;
+import skrueger.i8n.Translation;
+
+abstract public class QuantitiesRuleList<NUMBERTYPE extends Number> extends
+		FeatureRuleList {
+	private static final Logger LOGGER = Logger
+			.getLogger(QuantitiesRuleList.class);
+
+	/** KEY-name for the KVPs in the meta information * */
+	private static final String KVP_NORMALIZATION_FIELD = "NORM";
+
+	/** KEY-name for the KVPs in the meta information * */
+	private static final String KVP_VALUE_FIELD = "VALUE";
+
+	/**
+	 * The {@link String} name of the attribute which contains the quantity
+	 * values
+	 */
+	private String value_field_name;
+
+	/**
+	 * The {@link String} name of the attribute used to normalize the quantity
+	 * attribute
+	 */
+	private String normalizer_field_name;
+
+	private Color[] colors = null;
+
+	public int getNumClasses() {
+		return classLimits.size() - 1;
+	}
+
+	/** Caches the limits* */
+	private TreeSet<NUMBERTYPE> classLimits = new TreeSet<NUMBERTYPE>();
+
+	/**
+	 * Defines the number of digits shown in interval description (rule title);
+	 * Default is 3
+	 */
+	private int classDigits = 3;
+	private DecimalFormat classDigitsFormat = new DecimalFormat(SwingUtil
+			.getNumberFormatPattern(classDigits));
+
+	/**
+	 * Returns the number of digits shown in the rule description.
+	 */
+	public int getClassDigits() {
+		return this.classDigits;
+	}
+
+	/**
+	 * Sets the number of digits shown in the rule description. Values less then
+	 * 0 are treat as 0.
+	 */
+	public void setClassDigits(int classDigits) {
+		this.classDigits = Math.max(0, classDigits);
+		this.classDigitsFormat.applyPattern(SwingUtil
+				.getNumberFormatPattern(classDigits));
+	}
+
+	public TreeSet<NUMBERTYPE> getClassLimits() {
+		return classLimits;
+	}
+
+	/**
+	 * @return the classLimits as an {@link ArrayList}.
+	 * @author <a href="mailto:skpublic@wikisquare.de">Stefan Alfons
+	 *         Kr&uuml;ger</a>
+	 */
+	public ArrayList<NUMBERTYPE> getClassLimitsAsArrayList() {
+		return new ArrayList<NUMBERTYPE>(classLimits);
+	}
+
+	/**
+	 * This initializes {@link #numClasses} also. Colors are set to
+	 * <code>null</code>, so they will be chosen from the palette again. It
+	 * rests the rule titles.
+	 * 
+	 * @param classLimits
+	 *            Classlimits or the Classes to set.
+	 * 
+	 * @author <a href="mailto:skpublic@wikisquare.de">Stefan Alfons
+	 *         Kr&uuml;ger</a>
+	 */
+	public void setClassLimits(TreeSet<NUMBERTYPE> classLimits) {
+		setClassLimits(classLimits, false);
+	}
+
+	/**
+	 * This initializes {@link #numClasses} also. Colors are set to
+	 * <code>null</code>, so they will be chosen from the palette again.
+	 * 
+	 * @param classLimits
+	 *            Classlimits or the Classes to set.
+	 * @param classDigits
+	 *            number of digits shown in the rule title
+	 * @param resetRuleTitles
+	 *            if <code>true</code> the rule titles will be reset to default
+	 * 
+	 * @author <a href="mailto:skpublic@wikisquare.de">Stefan Alfons
+	 *         Kr&uuml;ger</a>
+	 */
+	public void setClassLimits(TreeSet<NUMBERTYPE> classLimits,
+			boolean resetRuleTitles) {
+
+		this.classLimits = classLimits;
+		if (classLimits.size() < 2) {
+			LOGGER.error("numClasses < 1 bei setClassLimits. NOT accepting it");
+			// numClasses = 0;
+			return;
+		}
+
+		/***********************************************************************
+		 * Create default Rule Titles..
+		 */
+		if (resetRuleTitles)
+			getRuleTitles().clear();
+
+		for (int i = 0; i < classLimits.size() - 1; i++) {
+			NUMBERTYPE lower = getClassLimitsAsArrayList().get(i);
+			NUMBERTYPE upper = getClassLimitsAsArrayList().get(i + 1);
+
+			String stringTitle = createDefaultClassLabelFor(lower, upper,
+					!(i < classLimits.size() - 1 - 1));
+
+			// If we do not reset the ruleTiles, we only put a default where no
+			// other value exists
+			if (!resetRuleTitles) {
+				if (!getRuleTitles().containsKey(i)) {
+					getRuleTitles().put(i, stringTitle);
+				}
+			} else {
+				getRuleTitles().put(i, stringTitle);
+			}
+
+		}
+
+		// Setting the colors to null we lead to new colors beeing created the
+		// next time getColors() is called.
+		setColors(null);
+
+		fireEvents(new RuleChangedEvent("Set class limits", this));
+	}
+
+	/**
+	 * Creates a default Rules Label for that class. It checks and honours the
+	 * {@link LANGUAGE_MODE} setting.
+	 * 
+	 * @param lower
+	 *            Lower (included) class break
+	 * @param upper
+	 *            Upper (excluded) class break
+	 * @param isLast
+	 *            If <code>true</code>, the upper class break is also included.
+	 * @return something like
+	 */
+	public String createDefaultClassLabelFor(Number lower, Number upper,
+			boolean isLast) {
+
+		String limitsLabel = "[" + classDigitsFormat.format(lower) + " - "
+				+ classDigitsFormat.format(upper);
+		limitsLabel += isLast ? "]" : "[";
+
+		String unit = getStyledFeatures().getAttributeMetaDataMap().get(
+				getValue_field_name()).getUnit();
+		if (unit != null && !unit.isEmpty())
+			limitsLabel += " "+ unit;
+
+		String stringTitle;
+		/**
+		 * Create a default title
+		 */
+		if (AtlasStyler.getLanguageMode() == AtlasStyler.LANGUAGE_MODE.ATLAS_MULTILANGUAGE) {
+			stringTitle = new Translation(AtlasStyler.getLanguages(),
+					limitsLabel).toOneLine();
+		} else {
+			stringTitle = limitsLabel;
+		}
+
+		return stringTitle;
+	}
+
+	public QuantitiesRuleList(StyledFeaturesInterface<?> styledFeatures) {
+		super(styledFeatures);
+		Collection<String> numericalFieldNames = FeatureUtil
+				.getNumericalFieldNames(getStyledFeatures().getSchema(), false);
+		if (numericalFieldNames.size() > 0)
+			value_field_name = numericalFieldNames.toArray(new String[] {})[0];
+	}
+
+	/**
+	 * @return the normalizer_field_name
+	 */
+	public String getNormalizer_field_name() {
+		return normalizer_field_name;
+	}
+
+	/**
+	 * @param normalizer_field_name
+	 *            the normalizer_field_name to set
+	 */
+	final public void setNormalizer_field_name(String normalizer_field_name) {
+		this.normalizer_field_name = normalizer_field_name;
+	}
+
+	/**
+	 * @return the value_field_name
+	 */
+	final public String getValue_field_name() {
+		return value_field_name;
+	}
+
+	/**
+	 * @param value_field_name
+	 *            the value_field_name to set
+	 */
+	public void setValue_field_name(String value_field_name) {
+		this.value_field_name = value_field_name;
+	}
+
+	/**
+	 * @param filter
+	 *            A {@link Filter}
+	 * @author <a href="mailto:skpublic@wikisquare.de">Stefan Alfons
+	 *         Kr&uuml;ger</a>
+	 * 
+	 * @return <code>null</code> if it is not a "BetweenFilter"
+	 */
+	public static double[] interpretBetweenFilter(Filter filter) {
+
+		if (filter instanceof AndImpl) {
+			// This is a AND ( NOT ( NODATA ) , BETWEENFILTER) construction
+			// We continue the interpretion with only the last filter
+			Iterator<Filter> fi = ((AndImpl) filter).getFilterIterator();
+			while (fi.hasNext())
+				filter = fi.next();
+		}
+
+		if (filter instanceof PropertyIsBetween) {
+			PropertyIsBetween betweenFilter = (PropertyIsBetween) filter;
+			double lower = Double.parseDouble(betweenFilter.getLowerBoundary()
+					.toString());
+			double upper = Double.parseDouble(betweenFilter.getUpperBoundary()
+					.toString());
+			return new double[] { lower, upper };
+		}
+		return null;
+	}
+
+	private HashMap<Integer, String> ruleTitles = new HashMap<Integer, String>();
+
+	public HashMap<Integer, String> getRuleTitles() {
+		return ruleTitles;
+	}
+
+	public void setRuleTitles(HashMap<Integer, String> ruleTitles) {
+		this.ruleTitles = ruleTitles;
+		fireEvents(new RuleChangedEvent("setRuleTitles", this));
+	}
+
+	/**
+	 * The children of this class define most metainfo. Some metainformation is
+	 * the same for all children and is added here.
+	 * 
+	 * @param metaInfoString
+	 *            The metaInfoString starting with a getType()-result and some
+	 *            KVPs
+	 * 
+	 * @return an expanded {@link String} with more KVPs
+	 * 
+	 * @author <a href="mailto:skpublic@wikisquare.de">Stefan Alfons
+	 *         Kr&uuml;ger</a>
+	 */
+	protected String extendMetaInfoString(String metaInfoString) {
+		metaInfoString += METAINFO_SEPERATOR_CHAR + KVP_VALUE_FIELD
+				+ METAINFO_KVP_EQUALS_CHAR + getValue_field_name();
+		metaInfoString += METAINFO_SEPERATOR_CHAR + KVP_NORMALIZATION_FIELD
+				+ METAINFO_KVP_EQUALS_CHAR + getNormalizer_field_name();
+		return metaInfoString;
+	}
+
+	/**
+	 * The children of this class parse most metainfo. Some metainformation is
+	 * the same for all children and is parsed here. This method must be called
+	 * from all children.
+	 * 
+	 * @param metaInfoString
+	 *            The metaInfoString cleared from with a getType()-result and
+	 *            some KVPs
+	 * 
+	 * @return an expanded {@link String} with more KVPs
+	 * 
+	 * @author <a href="mailto:skpublic@wikisquare.de">Stefan Alfons
+	 *         Kr&uuml;ger</a>
+	 */
+	protected void parseMetaInfoString(String metaInfoString) {
+
+		/***********************************************************************
+		 * Parsing a list of Key-Value Pairs from the FeatureTypeStyleName
+		 */
+		String[] kvp = new String[] { "uninitialized", "uninit" };
+		String value = "nothging yet";
+		try {
+
+			String[] params = metaInfoString.split(METAINFO_SEPERATOR_CHAR);
+			for (String p : params) {
+				kvp = p.split(METAINFO_KVP_EQUALS_CHAR);
+
+				if (kvp[0].equalsIgnoreCase(KVP_NORMALIZATION_FIELD.toString())) {
+					value = kvp[1];
+					if (value.equalsIgnoreCase("null"))
+						setNormalizer_field_name(null);
+					else
+						setNormalizer_field_name(FeatureUtil
+								.findBestMatchingAttribute(
+										getStyledFeatures().getSchema(), value)
+								.getLocalPart());
+				}
+
+				if (kvp[0].equalsIgnoreCase(KVP_VALUE_FIELD.toString())) {
+					value = kvp[1];
+					if (value.equalsIgnoreCase("null"))
+						setValue_field_name(null);
+					else
+						setValue_field_name(FeatureUtil
+								.findBestMatchingAttributeFallBackFirstNumeric(
+										getStyledFeatures().getSchema(), kvp[1])
+								.getLocalPart());
+				}
+			}
+
+		} catch (RuntimeException e) {
+			LOGGER.error("KVP=" + kvp[0] + kvp[1]);
+			LOGGER.error("VALUE=" + value);
+
+			throw (e);
+		}
+	}
+
+	/**
+	 * @param colors
+	 */
+	public void setColors(Color[] colors) {
+		this.colors = colors;
+	}
+
+	public Color[] getColors() {
+		return colors;
+	}
+
+	/***************************************************************************
+	 * ABSTRACT METHODS BEGIN HERE
+	 * 
+	 * @return
+	 **************************************************************************/
+
+	abstract public void parseMetaInfoString(String metaInfoString,
+			FeatureTypeStyle fts);
+
+}
