f1e5b545bc267f01d903975c001b53dd3e416bcf
alfonx <tzeggai@wikisquare.de>
2010-06-04 16:29:27 +0000

    Removing the "(soon changing to Stefan A. Tzeggai)" from the copyright notices

--- gpswinggui/src/main/java/org/geopublishing/geopublisher/swing/GpSwingUtil.java
+++ gpswinggui/src/main/java/org/geopublishing/geopublisher/swing/GpSwingUtil.java
@@ -1,13 +1,13 @@
 package org.geopublishing.geopublisher.swing;
 /*******************************************************************************
- * Copyright (c) 2010 Stefan A. Tzeggai (soon changing to Stefan A. Tzeggai).
+ * Copyright (c) 2010 Stefan A. Tzeggai.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Public License v2.0
  * which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  * 
  * Contributors:
- *     Stefan A. Tzeggai (soon changing to Stefan A. Tzeggai) - initial API and implementation
+ *     Stefan A. Tzeggai - initial API and implementation
  ******************************************************************************/
 
 
