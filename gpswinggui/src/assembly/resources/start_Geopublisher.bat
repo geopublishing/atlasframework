94f6f436e8b5fb40671dddfb513dedae9a98355b
alfonx <tzeggai@wikisquare.de>
2010-06-05 12:09:00 +0000

    Classpath seperation char is platform dependent! So the start...bat have to use ; instead of :

--- gpswinggui/src/assembly/resources/start_Geopublisher.bat
+++ gpswinggui/src/assembly/resources/start_Geopublisher.bat
@@ -10,4 +10,4 @@ rem  Sie sollten eine Kopie der GNU Lesser General Public License zusammen mit d
 
 echo Starting Geopublisher. You need to have at least Java Runtime Environment (JRE) Version 1.6.0.14 installed. 
 
-javaw -Xmx320m -Dfile.encoding=UTF8  -Djava.library.path=. -cp %HOMEPATH%\.Geopublishing:gpswinggui-${project.version}.jar org.geopublishing.geopublisher.swing.GeopublisherGUI
+javaw -Xmx320m -Dfile.encoding=UTF8  -Djava.library.path=. -cp %HOMEPATH%\.Geopublishing;gpswinggui-${project.version}.jar org.geopublishing.geopublisher.swing.GeopublisherGUI
