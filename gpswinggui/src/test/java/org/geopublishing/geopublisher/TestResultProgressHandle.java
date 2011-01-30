0e4309b25031b8fe0a3f867e63eda41d333140d5
alfonx <tzeggai@wikisquare.de>
2011-01-30 12:32:14 +0000

    Moved Michael's md5hash method to IOUtils

---
+++ gpswinggui/src/test/java/org/geopublishing/geopublisher/TestResultProgressHandle.java
@@ -0,0 +1,70 @@
+package org.geopublishing.geopublisher;
+
+import java.awt.Container;
+
+import org.apache.log4j.Logger;
+import org.netbeans.spi.wizard.ResultProgressHandle;
+
+/**
+ * This implementation of a {@link ResultProgressHandle} will pipe all output to
+ * the logger. If not {@link Logger} is given, it outputs to sysout
+ */
+public class TestResultProgressHandle implements ResultProgressHandle {
+
+	public TestResultProgressHandle() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public TestResultProgressHandle(Logger logger) {
+		this.logger = logger;
+	}
+
+	private Logger logger = null;
+
+	@Override
+	public void setProgress(int currentStep, int totalSteps) {
+
+	}
+
+	@Override
+	public void setProgress(String description, int currentStep, int totalSteps) {
+	}
+
+	@Override
+	public void setBusy(String description) {
+		final String msg = "progress busy: " + description;
+		if (logger != null) {
+			logger.debug(msg);
+		} else
+			System.out.println(msg);
+	}
+
+	@Override
+	public void finished(Object result) {
+		final String msg = "progress finished with: " + result;
+		if (logger != null) {
+			logger.debug(msg);
+		} else
+			System.out.println(msg);
+	}
+
+	@Override
+	public void failed(String message, boolean canNavigateBack) {
+		final String msg = "progress failed with: " + message;
+		if (logger != null) {
+			logger.error(msg);
+		} else
+			System.err.println(msg);
+
+	}
+
+	@Override
+	public void addProgressComponents(Container panel) {
+	}
+
+	@Override
+	public boolean isRunning() {
+		throw new RuntimeException("not yet implemented");
+	}
+
+}
--- gpswinggui/src/test/java/org/geopublishing/geopublisher/export/GpFtpAtlasExportTest.java
+++ gpswinggui/src/test/java/org/geopublishing/geopublisher/export/GpFtpAtlasExportTest.java
@@ -2,15 +2,13 @@ package org.geopublishing.geopublisher.export;
 
 import static org.junit.Assert.assertNotNull;
 
-import java.awt.Container;
-
 import org.geopublishing.geopublisher.AtlasConfigEditable;
 import org.geopublishing.geopublisher.GpTestingUtil;
+import org.geopublishing.geopublisher.TestResultProgressHandle;
 import org.geopublishing.gpsync.AtlasFingerprint;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.netbeans.spi.wizard.ResultProgressHandle;
 
 import de.schmitzm.lang.LangUtil;
 import de.schmitzm.testing.TestingClass;
@@ -38,66 +36,21 @@ public class GpFtpAtlasExportTest extends TestingClass {
 	@Test
 	public void testUpload() throws Exception {
 		AtlasConfigEditable ace = GpTestingUtil.TestAtlas.small.getAce();
-		ace.setBaseName("testUpload_" + System.currentTimeMillis());
+		ace.setBaseName("testUpload-" + System.currentTimeMillis());
 		GpFtpAtlasExport gpFtpAtlasExport = new GpFtpAtlasExport(ace);
 		AtlasFingerprint requestFingerprint = gpFtpAtlasExport
 				.requestFingerprint(null);
 		System.out.println(requestFingerprint);
-		gpFtpAtlasExport.export(progress);
+
+		gpFtpAtlasExport.export(new TestResultProgressHandle());
 
 		// wait a minute for gphoster to locate the zip
 		Thread.sleep(LangUtil.MIN_MILLIS * 2);
 
 		requestFingerprint = gpFtpAtlasExport.requestFingerprint(null);
-		assertNotNull("After the upload the atlas must have a fingerprint",
+		assertNotNull("After the upload the atlas should have a fingerprint",
 				requestFingerprint);
 
 	}
-
-	private final ResultProgressHandle progress = new ResultProgressHandle() {
-
-		@Override
-		public void setProgress(String description, int currentStep,
-				int totalSteps) {
-			// TODO Auto-generated method stub
-
-		}
-
-		@Override
-		public void setProgress(int currentStep, int totalSteps) {
-			// TODO Auto-generated method stub
-
-		}
-
-		@Override
-		public void setBusy(String description) {
-			// TODO Auto-generated method stub
-
-		}
-
-		@Override
-		public boolean isRunning() {
-			// TODO Auto-generated method stub
-			return false;
-		}
-
-		@Override
-		public void finished(Object result) {
-			// TODO Auto-generated method stub
-
-		}
-
-		@Override
-		public void failed(String message, boolean canNavigateBack) {
-			// TODO Auto-generated method stub
-
-		}
-
-		@Override
-		public void addProgressComponents(Container panel) {
-			// TODO Auto-generated method stub
-
-		}
-	};
 
 }
