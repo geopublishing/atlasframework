13703e5d1dcf05f31c1b052f6c189c7efd6d7282
mojays <mojays@daefa287-774f-0410-9f13-96a2406eee74>
2011-03-17 20:50:51 +0000

    handling of SWT library in start_Geopublisher.bat

--- gpswinggui/src/assembly/resources/start_Geopublisher.bat
+++ gpswinggui/src/assembly/resources/start_Geopublisher.bat
@@ -1,4 +1,6 @@
 @echo off
+setlocal
+
 echo Copyright 2008 Stefan Alfons Tzeggai
 rem atlas-framework - This file is part of the Atlas Framework
 rem This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation; either version 2.1 of the License, or (at your option) any later version.
@@ -7,4 +9,16 @@ rem  You should have received a copy of the GNU Lesser General Public License al
 
 echo "Starting Geopublisher..." 
 
-javaw -Xmx320m -Dfile.encoding=UTF8  -Djava.library.path=. -cp "%HOMEPATH%\.Geopublishing;gpswinggui-${project.version}.jar" org.geopublishing.geopublisher.swing.GeopublisherGUI %~f1 %~f2 %~f3 %~f4 %~f5 %~f6 %~f7 %~f8 %~f9 %~f10 %~f11 %~f12 %~f13
+set SWTJAR=swt-windows-3.6-x86.jar
+
+md %HOMEPATH%\.Geopublishing > dummy
+java -version 2>%HOMEPATH%\.Geopublishing\javaversion.txt
+
+FINDSTR "64" %HOMEPATH%\.Geopublishing\javaversion.txt
+if %ERRORLEVEL% EQU 0 set SWTJAR=swt-windows-3.6-x86_64.jar
+echo. Using native SWT library %SWTJAR%
+
+javaw -Xmx320m -Dfile.encoding=UTF8  -Djava.library.path=. -cp "%SWTJAR%;%HOMEPATH%\.Geopublishing;gpswinggui-${project.version}.jar" org.geopublishing.geopublisher.swing.GeopublisherGUI %~f1 %~f2 %~f3 %~f4 %~f5 %~f6 %~f7 %~f8 %~f9 %~f10 %~f11 %~f12 %~f13
+
+endlocal
+
