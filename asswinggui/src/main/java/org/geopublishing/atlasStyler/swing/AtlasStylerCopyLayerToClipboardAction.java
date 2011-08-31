f25a62d71228f2ee0152c53345826edef41a4506
keeb <keeb@daefa287-774f-0410-9f13-96a2406eee74>
2011-08-31 20:16:18 +0000

    AtlasStyler now makes it possible to use our optimized version for sld export to clipboard.

--- asswinggui/src/main/java/org/geopublishing/atlasStyler/swing/AtlasStylerCopyLayerToClipboardAction.java
+++ asswinggui/src/main/java/org/geopublishing/atlasStyler/swing/AtlasStylerCopyLayerToClipboardAction.java
@@ -17,16 +17,19 @@ import java.io.FileInputStream;
 import java.util.List;
 
 import javax.swing.AbstractAction;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 import org.apache.log4j.Logger;
 import org.geopublishing.atlasStyler.ASUtil;
 import org.geopublishing.atlasViewer.swing.Icons;
-
+import org.geopublishing.geopublisher.GpUtil;
 import de.schmitzm.geotools.styling.StyledLayerInterface;
 import de.schmitzm.geotools.styling.StylingUtil;
 import de.schmitzm.io.IOUtil;
 import de.schmitzm.lang.LangUtil;
 import de.schmitzm.swing.ExceptionDialog;
+import de.schmitzm.swing.SwingUtil;
 
 public class AtlasStylerCopyLayerToClipboardAction extends AbstractAction {
 	private static final long serialVersionUID = 4726448851995462364L;
@@ -52,12 +55,28 @@ public class AtlasStylerCopyLayerToClipboardAction extends AbstractAction {
 		File tempFile = null;
 		try {
 			tempFile = File.createTempFile("atlasStylerClipboard", ".sld");
+			SwingUtil.checkOnEDT();
 
-			// TODO MJ:
-			// Benutezr fragen, ob er die Normale oder Optimierte/Produktion Version Kopieren will. Wenn productino, dann:;
-//			StylingUtil.saveStyleToSld(StylingUtil.optimizeStyle(styledLayer.getStyle(), AtlasStylerSaveAsLayerToSLDAction.getOptimizedTitle(styledLayer)), tempFile);
-			// sonst:
-			StylingUtil.saveStyleToSld(styledLayer.getStyle(), tempFile);
+			Object[] options = {
+					ASUtil.R("ExportToClipboard.Normal"),
+					ASUtil.R("ExportToClipboard.Optimized") };
+
+			int versionToCopy = JOptionPane
+					.showOptionDialog(
+							owner,
+							ASUtil.R("ExportToClipboard.ProductiveOrNormalVersion.msg"),
+							ASUtil.R("ExportToClipboard.ProductiveOrNormalVersion.title"),
+							JOptionPane.YES_NO_OPTION,
+							JOptionPane.QUESTION_MESSAGE, null, options,
+							options[0]);
+
+			if (versionToCopy == JOptionPane.YES_OPTION)
+				StylingUtil.saveStyleToSld(styledLayer.getStyle(), tempFile);
+			else if (versionToCopy == JOptionPane.NO_OPTION)
+				StylingUtil.saveStyleToSld(StylingUtil.optimizeStyle(
+						styledLayer.getStyle(),
+						AtlasStylerSaveAsLayerToSLDAction
+								.getOptimizedTitle(styledLayer)), tempFile);
 
 			List<Exception> es = StylingUtil.validateSld(new FileInputStream(
 					tempFile));
