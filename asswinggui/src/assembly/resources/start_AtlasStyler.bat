5fb5d41b78747cb37d58ab5d209b7d8c50cea99c
alfonx <tzeggai@wikisquare.de>
2012-05-01 12:58:22 +0000

    AS mit SWT dependency? https://trac.wikisquare.de/gp/ticket/84

--- asswinggui/src/assembly/resources/start_AtlasStyler.bat
+++ asswinggui/src/assembly/resources/start_AtlasStyler.bat
@@ -1,10 +1,7 @@
 @echo off
 startlocal
 
-set HOMEPATH_COMPL=%HOMEDRIVE%%HOMEPATH%
-set JAI=jai_codec-windows-1.1.3.jar;jai_core-windows-1.1.3.jar;mlibwrapper_jai-windows-1.1.3.jar
-
-echo Copyright 2010 Stefan Alfons Tzeggai
+echo Copyright 2012 Stefan Alfons Tzeggai
 rem atlas-framework - This file is part of the Atlas Framework
 rem This library is free software; you can redistribute it and/or modify it under the terms of the GNU  Lesser General Public License as published by the Free Software Foundation; either version 2.1 of the  License, or (at your option) any later version.
 rem This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without  even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser  General Public License for more details.
@@ -12,16 +9,10 @@ rem  You should have received a copy of the GNU Lesser General Public License al
 echo "Starting AtlasStyler..."
 
 set HOMEPATH_COMPL=%HOMEDRIVE%%HOMEPATH%
-set SWTJAR=swt-windows-${swt.version}-x86.jar
 set JAI=jai_codec-windows-1.1.3.jar;jai_core-windows-1.1.3.jar;mlibwrapper_jai-windows-1.1.3.jar
 
 md "%HOMEPATH_COMPL%\.Geopublishing"
-java -version 2>"%HOMEPATH_COMPL%\.Geopublishing\javaversion.txt"
 
-FINDSTR "64" "%HOMEPATH_COMPL%\.Geopublishing\javaversion.txt"
-if %ERRORLEVEL% EQU 0 set SWTJAR=swt-windows-${swt.version}-x86_64.jar
-echo.Using native SWT library %SWTJAR%
-
-javaw -Xmx500m -Dfile.encoding=UTF-8 -Djava.library.path=. -cp "%HOMEPATH%\.Geopublishing;%JAI%;%SWTJAR%;asswinggui-${project.version}.jar" org.geopublishing.atlasStyler.swing.AtlasStylerGUI %~f1 %~f2 %~f3 %~f4 %~f5 %~f6 %~f7 %~f8 %~f9 %~f10 %~f11 %~f12 %~f13 
+javaw -Xmx500m -Dfile.encoding=UTF-8 -Djava.library.path=. -cp "%HOMEPATH%\.Geopublishing;%JAI%;asswinggui-${project.version}.jar" org.geopublishing.atlasStyler.swing.AtlasStylerGUI %~f1 %~f2 %~f3 %~f4 %~f5 %~f6 %~f7 %~f8 %~f9 %~f10 %~f11 %~f12 %~f13 
 
 endlocal
--- asswinggui/src/assembly/resources/start_AtlasStyler.command
+++ asswinggui/src/assembly/resources/start_AtlasStyler.command
@@ -1,23 +1,14 @@
 #!/bin/bash
-# Copyright 2011 Stefan Alfons Tzeggai
+# Copyright 2012 Stefan Alfons Tzeggai
 # atlas-framework - This file is part of the Atlas Framework
 # This library is free software; you can redistribute it and/or modify it under the terms of the GNU  Lesser General Public License as published by the Free Software Foundation; either version 2.1 of the  License, or (at your option) any later version.
 # This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without  even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser  General Public License for more details.
 #  You should have received a copy of the GNU Lesser General Public License along with this library;  if not, write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110, USA
 echo "Starting AtlasStyler..."
 
-export SWTJAR="swt-gtk-linux-${swt.version}-x86.jar"
-
 export JAI=jai_core-linux-1.1.3.jar:jai_codec-linux-1.1.3.jar
 
 mkdir ~/.Geopublishing &> /dev/null
-java -version &> ~/.Geopublishing/javaversion
 
-if [[ `cat ~/.Geopublishing/javaversion` == *64-Bit* ]]
-then
-  export SWTJAR="swt-gtk-linux-${swt.version}-x86_64.jar"
-  echo "Using 64bit SWT native libs for 64-bit Java..."
-fi
-
-java -Xmx160m -Dfile.encoding=UTF8 -Djava.library.path=. -cp $SWTJAR:$JAI:~/.Geopublishing:asswinggui-${project.version}.jar org.geopublishing.atlasStyler.swing.AtlasStylerGUI ${1} ${2} ${3} ${4} ${5} ${6} ${7} ${8} ${9} ${10} ${11} ${12} ${13} ${14} ${15} ${16} ${17}
+java -Xmx160m -Dfile.encoding=UTF8 -Djava.library.path=. -cp $JAI:~/.Geopublishing:asswinggui-${project.version}.jar org.geopublishing.atlasStyler.swing.AtlasStylerGUI ${1} ${2} ${3} ${4} ${5} ${6} ${7} ${8} ${9} ${10} ${11} ${12} ${13} ${14} ${15} ${16} ${17}
 
