c18056a6279049f4e8543a6ef85a776342fbf400
alfonx <tzeggai@wikisquare.de>
2012-06-10 19:14:36 +0000

    Ignored gpnatives and comittet some pom.xml updated made ma m2eclipse

--- gpswinggui/src/main/java/org/geopublishing/geopublisher/gui/map/MapPoolAddAction.java
+++ gpswinggui/src/main/java/org/geopublishing/geopublisher/gui/map/MapPoolAddAction.java
@@ -37,32 +37,6 @@ public class MapPoolAddAction extends AbstractAction {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-//
-//		// Setup empty translations
-//		Translation name, desc, keywords;
-//		List<String> languages = mapPoolJTable.getAce().getLanguages();
-//		name = new Translation(languages, Geopublisher
-//				.R("MapPool.DummyMapName"));
-//		desc = new Translation(languages, Geopublisher
-//				.R("MapPool.DummyMapDescription"));
-//		keywords = new Translation();
-//
-//		final TranslationEditJPanel namePanel = new TranslationEditJPanel(
-//				Geopublisher.R("MapPreferences_translateTheMapsName"), name,
-//				languages);
-//		final TranslationEditJPanel descPanel = new TranslationEditJPanel(
-//				Geopublisher.R("MapPreferences_translateTheMapsDescription"),
-//				desc, languages);
-//		final TranslationEditJPanel keywordsPanel = new TranslationEditJPanel(
-//				Geopublisher.R("MapPreferences_translateTheMapsKeywords"),
-//				desc, languages);
-//
-//		TranslationAskJDialog translationAskJDialog = new TranslationAskJDialog(
-//				mapPoolJTable, namePanel, descPanel, keywordsPanel);
-//		translationAskJDialog.setVisible(true);
-//		if (translationAskJDialog.isCancelled()) {
-//			return;
-//		}
 
 		// The map is created and automatically added to the mappool.
 		Map newMap = new Map(mapPoolJTable.getAce());
