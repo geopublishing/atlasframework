baf234d05f14bf323d2e4a9634514b0e76fcab65
keeb <keeb@daefa287-774f-0410-9f13-96a2406eee74>
2012-07-28 18:35:12 +0000

    tests work

--- gpcore/src/test/java/org/geopublishing/geopublisher/CopyPasteClipboardTest.java
+++ gpcore/src/test/java/org/geopublishing/geopublisher/CopyPasteClipboardTest.java
@@ -3,19 +3,25 @@ package org.geopublishing.geopublisher;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.io.IOException;
 import java.util.Set;
 
 import javax.xml.transform.TransformerException;
 
 import org.geopublishing.atlasStyler.AtlasStylerRaster;
+import org.geopublishing.atlasStyler.AtlasStylerVector;
 import org.geopublishing.atlasStyler.rulesLists.RasterRulesListRGB;
+import org.geopublishing.atlasStyler.rulesLists.UniqueValuesPolygonRuleList;
 import org.geopublishing.atlasViewer.dp.layer.DpLayer;
 import org.geopublishing.geopublisher.GpTestingUtil.TestAtlas;
+import org.geotools.data.FeatureSource;
 import org.geotools.styling.FeatureTypeStyle;
 import org.geotools.styling.Style;
 import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
 
 import de.schmitzm.geotools.styling.StyledGridCoverageReader;
 import de.schmitzm.geotools.styling.StylingUtil;
@@ -25,15 +31,10 @@ import de.schmitzm.lang.LangUtil;
 public class CopyPasteClipboardTest {
 
 	private static AtlasConfigEditable ace;
-	private static AtlasConfigEditable acr;
 
 	@BeforeClass
 	public static void setup() throws Exception {
 		ace = GpTestingUtil.getAtlasConfigE(TestAtlas.small);
-		acr = GpTestingUtil.getAtlasConfigE(TestAtlas.rasters);
-		Style testStyle = StylingUtil.STYLE_FACTORY.createStyle();
-		testStyle.featureTypeStyles().add(
-				StylingUtil.STYLE_FACTORY.createFeatureTypeStyle());
 	}
 
 	@Test
@@ -61,27 +62,52 @@ public class CopyPasteClipboardTest {
 		assertFalse(StylingUtil.isStyleDifferent(oldStyle, loadedStyle[0]));
 	}
 
-	@Ignore
 	@Test
 	public void testCreateRasterLayerFromPaste() throws Exception {
 		StyledGridCoverageReader styledRaster = GTTestingUtil.TestDatasetsRaster.geotiffWithSld
 				.getStyled();
 		RasterRulesListRGB rl = new RasterRulesListRGB(styledRaster, true);
-		AtlasStylerRaster asv = new AtlasStylerRaster(styledRaster);
-		asv.addRulesList(rl);
-		asv.importStyle(asv.getStyle()); // selfimport damit die Defaultdaten im
+		AtlasStylerRaster asr = new AtlasStylerRaster(styledRaster);
+		asr.addRulesList(rl);
+		asr.importStyle(asr.getStyle()); // selfimport damit die Defaultdaten im
 											// TestDatasetsRaster überschrieben
 											// werden
-		LangUtil.copyToClipboard(StylingUtil.toXMLString(asv.getStyle()));
+
+		LangUtil.copyToClipboard(StylingUtil.toXMLString(asr.getStyle()));
 
 		String pastedXml = LangUtil.pasteFromClipboard();
 
 		Style[] pastedSLD = StylingUtil.loadSLD(pastedXml);
 		StyledGridCoverageReader styledRaster2 = GTTestingUtil.TestDatasetsRaster.arcAscii
 				.getStyled();
-		AtlasStylerRaster asr = new AtlasStylerRaster(styledRaster2);
-		asr.importStyle(pastedSLD[0]);
-		assertFalse(StylingUtil
-				.isStyleDifferent(asr.getStyle(), asv.getStyle()));
+		AtlasStylerRaster asr2 = new AtlasStylerRaster(styledRaster2);
+		asr2.importStyle(pastedSLD[0]);
+		assertFalse(StylingUtil.isStyleDifferent(asr2.getStyle(),
+				asr.getStyle()));
+	}
+
+	@Test
+	public void testCreateVectorLayerFromPaste() throws Exception {
+		FeatureSource<SimpleFeatureType, SimpleFeature> featureSource_polygon = GTTestingUtil.TestDatasetsVector.countryShp
+				.getFeatureSource();
+		AtlasStylerVector asv = new AtlasStylerVector(featureSource_polygon);
+		UniqueValuesPolygonRuleList uniqueRL1 = asv.getRlf()
+				.createUniqueValuesPolygonRulesList(true);
+		asv.addRulesList(uniqueRL1);
+		asv.importStyle(asv.getStyle()); // selfimport damit defaultdaten
+											// überschrieben werden
+
+		LangUtil.copyToClipboard(StylingUtil.toXMLString(asv.getStyle()));
+
+		String pastedXml = LangUtil.pasteFromClipboard();
+
+		Style[] pastedSLD = StylingUtil.loadSLD(pastedXml);
+		
+		FeatureSource<SimpleFeatureType, SimpleFeature> featureSource_kreise = GTTestingUtil.TestDatasetsVector.kreise
+				.getFeatureSource();
+		
+		AtlasStylerVector asv2 = new AtlasStylerVector(featureSource_kreise);
+		asv2.importStyle(pastedSLD[0]);
+		assertFalse(StylingUtil.isStyleDifferent(asv2.getStyle(), asv.getStyle()));
 	}
 }
